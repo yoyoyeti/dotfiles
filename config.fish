0f759b96a5d4b78872a9f3b0bf0b74abe7a0c9f6
Zachary Harwood <9059772+lobstrr@users.noreply.github.com>
2018-03-16 13:22:41 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,9 +3,7 @@ thefuck --alias | source
 set -x GOPATH $HOME/workspace/go
 set -x PATH $PATH $GOPATH/bin
 
-/*makes fish prompt colors different*/
-set pure_color_blue (set_color "638ffa")
-set pure_color_green (set_color "84fba2")
+set -g theme_short_path yes
 
 alias npmcis="npm run clean ;and npm i ;and npm start"
 alias gpom="git pull origin master"
