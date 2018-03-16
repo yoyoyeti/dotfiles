f1fa2430eda5a275d226bbb96e26f8dcece5d9f9
Zachary Harwood <9059772+lobstrr@users.noreply.github.com>
2018-03-16 13:23:40 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,5 +5,7 @@ set -x PATH $PATH $GOPATH/bin
 
 set -g theme_short_path yes
 
+set fish_color_cwd ff7675
+
 alias npmcis="npm run clean ;and npm i ;and npm start"
 alias gpom="git pull origin master"
