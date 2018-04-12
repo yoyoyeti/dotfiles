e792c43c7f19010edc14d1f30000ded01fb40e79
Zachary Harwood <9059772+lobstrr@users.noreply.github.com>
2018-04-12 09:29:47 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,11 +1,15 @@
 thefuck --alias | source
 
 set -x GOPATH $HOME/workspace/go
-set -x PATH $PATH $GOPATH/bin
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+set -x SCALA_HOME /usr/local/share/scala-2.11.11
+set -x ZINC_HOME /usr/local/share/zinc-0.3.13
+set -x APP_HOME $HOME/workspace/anthology
+
+
+set -x PATH $PATH $GOPATH/bin $SCALA_HOME/bin $ZINC_HOME/bin $APP_HOME
 
 set -g theme_short_path yes
-
-set fish_color_cwd ff7675
 
 alias npmcis="npm run clean ;and npm i ;and npm start"
 alias gpom="git pull origin master"
